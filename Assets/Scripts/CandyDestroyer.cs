f3151133d6f441dad4cdb825d035e4a88ad0c812
cf <hoge>
2022-02-02 14:45:22 +0900

    Gold追加

--- Assets/Scripts/CandyDestroyer.cs
+++ Assets/Scripts/CandyDestroyer.cs
@@ -7,6 +7,7 @@ public class CandyDestroyer : MonoBehaviour
     public CandyManager candyManager;
     public int reward;
     public GameObject effectPrefab;
+    public GameObject effectGoldPrefab;
     public Vector3 effectRotation;
 
     private void OnTriggerEnter(Collider other)
@@ -23,22 +24,35 @@ public class CandyDestroyer : MonoBehaviour
             Destroy(other.gameObject);
                 //otherがなかったらCandyDestroyerが消えちゃう
 
-            if(effectPrefab != null)//
+            if(effectPrefab !=null && effectGoldPrefab != null)
+                //横に落ちた時がNull
             {
-                //Candyのポジションにエフェクトを生成
-                Instantiate(
-                    effectPrefab,//何を（パーティクル）
-                    other.transform.position,//どこに
-                    //other=destroyerに入ってきたcandy
-                    //otherはcube(candy)
-                    Quaternion.Euler(effectRotation)
-                    //どの向きで
-                    //effectRotation入れないと0,0,0で生成しちゃう
-                    //effectRotationはVector3でInspectorから登録した90
-                    //Quaternionは回転情報
-                    //どの向きに物体を回転させるか（どの向きで登場させるか）
-                    //Quaternionは４次元数
-                    );
+                if (other.gameObject.tag == "Candy")
+                {
+
+                    //Candyのポジションにエフェクトを生成
+                    Instantiate(
+                        effectPrefab,//何を（パーティクル）
+                        other.transform.position,//どこに
+                                                 //other=destroyerに入ってきたcandy
+                                                 //otherはcube(candy)
+                        Quaternion.Euler(effectRotation)
+                        //どの向きで
+                        //effectRotation入れないと0,0,0で生成しちゃう
+                        //effectRotationはVector3でInspectorから登録した90
+                        //Quaternionは回転情報
+                        //どの向きに物体を回転させるか（どの向きで登場させるか）
+                        //Quaternionは４次元数
+                        );
+                }
+                else
+                {
+                    Instantiate(
+                                effectGoldPrefab,
+                                other.transform.position,
+                                 Quaternion.Euler(effectRotation)
+                                 );
+                }
             }
             }
     }
